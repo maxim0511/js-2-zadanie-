6eac8438dc9036b44ea494fe5557d9f98796a972
Максим <max.volkov.2003@list.ru>
2022-04-20 18:37:24 +0300

    comm

--- javascript/index.js
+++ javascript/index.js
@@ -2,23 +2,22 @@ const API_KEY = 'dfedc326b45bf08db4593a892a7523d7';
 const BASE_URL ='https://api.themoviedb.org/';
 const LANGUAGE='en-US';
 const btn = document.querySelector('.header_search_btn');
+const pagination=document.querySelector('.pagination');
+const currentPageText = document.querySelector('.currentPage');
 let paramSearch = document.querySelector('.header_search_Input');
 let content = document.querySelector('.content_container');
 let htmlContent='';
+let pagin='';
 let page=0;
 
-const scrollContent = (e)=>{
-    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight)<10){
-        if (paramSearch.value == ''){
-            paramSearch.value='a'
-        }   
-        getContent(paramSearch.value,page+=1);
-    }
-}
 const getContent = async(query='a',page=1)=>{
+    window.scrollTo(0,0);
     try{
         const requset =await fetch(BASE_URL+`/3/search/collection?api_key=${API_KEY}&query=${query}&language=${LANGUAGE}&page=${page}`);
         const response =await requset.json();
+        htmlContent='';
+        pagin='';
+        currentPageText.innerHTML=`Выбранная страница:${page}`
         if (response.results.length == 0) {
             htmlContent=`<span class='NoItem'>Ничего не найдено...</span>`
         } 
@@ -33,13 +32,40 @@ const getContent = async(query='a',page=1)=>{
                             <div><p class="description">${overview}</p></div>
                         </div>`
             });     
+            for(let i=1;i<=(Math.ceil(response.total_results / response.results.length));i++){
+                pagin+=`
+                    <button class="pag_item" onclick="SetPage(${i})">${i}<button/>
+                `
+            }
         }   
+        pagination.innerHTML=pagin
         content.innerHTML = htmlContent;
     }
     catch(error){
         console.log(error);
     }
 }
+function SetPage(currentPage){
+    if (paramSearch.value == ''){
+        getContent('a',page=currentPage)
+    } else {
+        getContent(paramSearch.value,page=currentPage)
+    }
+}
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+function Input(){
+    htmlContent='';
+    valueSearch();
+}
+
+const NewContent = debounce(() => Input());
+  
 function valueSearch(){
     if (paramSearch.value == ''){
         getContent()
@@ -47,14 +73,7 @@ function valueSearch(){
         getContent(paramSearch.value)
     }
 }
-valueSearch()
+valueSearch();
 
-const NewContent = ()=>{
-        htmlContent='';
-        valueSearch();
-}
-
-btn.addEventListener('click',valueSearch());
-document.addEventListener('scroll',scrollContent);
-btn.removeEventListener('click',valueSearch());
-document.removeEventListener('scroll',scrollContent);
+btn.addEventListener('click',valueSearch);
+btn.removeEventListener('click',valueSearch);
